8ee83e38e55881ac2a7d2e9351beae33719b5613
planeset94 <planeset94@gmail.com>
2021-05-19 17:01:43 +0200

    solved v-on:click && add EventListener for Enter K

--- asset/JS/main.js
+++ asset/JS/main.js
@@ -4,35 +4,44 @@ const app = new Vue(
         el: '#root',
 
         data: {
+
             newTask: '',
+            editable: true,
             listToDo: [
                 'Lavare i piatti',
                 'fare esercizio',
 
-            ],
-            methods: {
-                addTask() {
+            ]
 
+        },
+
+        methods: {
+            addTask() {
+                if (this.newTask !== '') {
                     this.listToDo.push(this.newTask)
-
-
-
-                },
-
-
-
-
-
-
+                    this.newTask = ""
+                }
             },
+        },
 
-
-
+        mounted() {
+            document.addEventListener('keydown', (e) => {
+                const key = e.key;
+                if (key === 'Enter') {
+                    app.addTask()
+                }
+            })
 
         }
 
 
-
     }
+
+
+
+
+
+
+
 
 );
